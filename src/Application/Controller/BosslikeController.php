e710a36a436466d25265f3a3c486005f2569043a
Vladimir Link <melti-santi@mail.ru>
2020-09-26 16:23:51 +0200

    i am on it

--- src/Application/Controller/BosslikeController.php
+++ src/Application/Controller/BosslikeController.php
@@ -2,16 +2,39 @@
 
 namespace Bosslike\Application\Controller;
 
+use GuzzleHttp\ClientInterface;
+use GuzzleHttp\Exception\GuzzleException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class BosslikeController extends AbstractController
 {
     /**
+     * @var ClientInterface
+     */
+    private $restClient;
+
+    /**
+     * BosslikeController constructor.
+     *
+     * @param ClientInterface $restClient
+     */
+    public function __construct(ClientInterface $restClient)
+    {
+        $this->restClient = $restClient;
+    }
+
+    /**
      * @Route("/bosslike", name="bosslike")
      */
     public function index()
     {
+        try {
+            dd($this->restClient->get('https://httpbin.org/post'));
+        } catch (GuzzleException $exception) {
+            dd($exception->getMessage());
+        }
+
         return $this->json([
             'message' => 'Welcome to your new controller!',
             'path' => 'src/Controller/BosslikeController.php',
